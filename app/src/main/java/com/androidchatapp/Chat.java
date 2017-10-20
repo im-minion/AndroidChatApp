96449a5aeddddb8bc24e564c21f3c74b737dde75
im-minion <vaibhavminiyar@gmail.com>
2017-10-20 23:44:04 +0530

    removed comments

--- app/src/main/java/com/androidchatapp/Chat.java
+++ app/src/main/java/com/androidchatapp/Chat.java
@@ -111,42 +111,6 @@ public class Chat extends AppCompatActivity {
 
             }
         });
-
-//        chatRef1.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-//                Map map = dataSnapshot.getValue(Map.class);
-//                String message = map.get("message").toString();
-//                String userName = map.get("user").toString();
-//
-//                if(userName.equals(UserDetails.username)){
-//                    addMessageBox("You:-\n" + message, 1);
-//                }
-//                else{
-//                    addMessageBox(UserDetails.chatWith + ":-\n" + message, 2);
-//                }
-//            }
-//
-//            @Override
-//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(FirebaseError firebaseError) {
-//
-//            }
-//        });
     }
 
     public void addMessageBox(String message, int type) {
